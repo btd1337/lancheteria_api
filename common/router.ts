67aac605017dfcdd1f219d106cbbac713a040ef4
btd1337 <helder.bertoldo@ice.ufjf.br>
2018-06-23 13:07:56 -0300

    refactor router render

--- common/router.ts
+++ common/router.ts
@@ -1,5 +1,18 @@
+import { EventEmitter } from 'events';
 import * as restify from 'restify';
 
-export abstract class Router {
+export abstract class Router extends EventEmitter {
 	public abstract applyRoutes(application: restify.Server): any;
+
+	public render(response: restify.Response, next: restify.Next) {
+		return (document)=> {
+			if (document) {
+				this.emit('beforeRender', document);
+				response.json(document);
+			}else {
+				response.send(404);
+			}
+			return next();
+		};
+	}
 }
--- users/users.router.ts
+++ users/users.router.ts
@@ -3,36 +3,28 @@ import { Router } from '../common/router';
 import { User } from './users.model';
 
 class UsersRouter extends Router {
+
+  constructor(){
+    super();
+    this.on('beforeRender', document => {
+      document.password = undefined;
+    });
+  }
   public applyRoutes(application: restify.Server) {
     // getAll
     application.get('/users', (req, resp, next) => {
-      User.find().then(users => {
-        resp.json(users);
-        return next();
-      });
+      User.find().then(this.render(resp, next));
     });
 
     // getOne
     application.get('/users/:id', (req, resp, next) => {
-      User.findById(req.params.id).then(user => {
-        if (user) {
-          resp.json(user);
-          return next();
-        } else {
-          resp.send(400);
-          return next();
-        }
-      });
+      User.findById(req.params.id).then(this.render(resp,next));
     });
 
     // createOne
     application.post('/users', (req, resp, next) => {
       const user = new User(req.body);
-      user.save().then(currentUser => {
-        currentUser.password = undefined;
-        resp.json(user);
-        return next();
-      });
+      user.save().then(this.render(resp, next));
     });
 
     // editOne
@@ -48,37 +40,16 @@ class UsersRouter extends Router {
             }else{
               resp.send(404);
             }
-      }).then(user =>{
-        resp.json(user);
-        return next();
-      });
+      }).then(this.render(resp, next));
     });
 
     // editOne
     application.patch('/users/:id', (req, resp, next) => {
       const options = { new: true };  // to receive new object created
       User.findByIdAndUpdate(req.params.id, req.body, options)
-        .then(user => {
-          if(user) {
-            resp.json(user);
-            return next();
-          } else {
-            resp.send(404);
-            return next();
-          }
-        });
+        .then(this.render(resp, next));
     });
 
-    /* application.del('/users/:id', (req, resp, next)=> {
-      User.deleteOne({_id:req.params.id}).exec().then((cmdResult:any) => {
-        if(cmdResult.result.n){
-          resp.send(204);
-        } else{
-          resp.send(404);
-        }
-        return next();
-      });
-    }); */
     application.del('/users/:id', (req, resp, next)=> {
       User.deleteOne({_id:req.params.id}).exec().then(cmdResult => {
         if(cmdResult.n){  // n=1 found | n=0 not found
